9fe97978c38e28983b9bd1b812b5226d0dfaad84
SFRMilkTea <bayliz@bk.ru>
2023-12-24 17:12:02 +0700

    готовый дашборд

--- main.py
+++ main.py
@@ -1,119 +1,172 @@
-import matplotlib.pyplot as plt
-import seaborn as seaborn
 import streamlit as st
+import plotly.express as px
 import pandas as pd
 
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.feature_extraction.text import CountVectorizer
-st.title('крутой классный дашборд')
-# Загрузим данные из Excel-файла
-file_path = 'data/data.xlsx'
-# data_load_state = st.text('Мы загружаемся!')
-orders_df = pd.read_excel(file_path, sheet_name='Orders')
+file_path = 'data/data.xlsx'  # исходный файл
+st.set_page_config(page_title='Dashboard', layout='wide')
+st.title('Анализ данных о продажах розничной компании')
+st.markdown('<style>div.block-container{padding-top:1rem;}<style>', unsafe_allow_html=True)
+# считывание данных в датафрейм
+df = pd.read_excel(file_path)
 returns_df = pd.read_excel(file_path, sheet_name='Returns')
+# панель фильтрации
+st.sidebar.header("Фильтрация")
+# поля фильтрации
+ship_mode = st.sidebar.multiselect(
+    "Выберите способ доставки:",
+    options=df["Ship Mode"].unique(),
+)
+category = st.sidebar.multiselect(
+    "Выберите категорию продуктов:",
+    options=df["Category"].unique(),
+)
+region = st.sidebar.multiselect(
+    "Выберите регион:",
+    options=df["Region"].unique(),
+)
+country = st.sidebar.multiselect(
+    "Выберите страну:",
+    options=df["Country"].unique(),
+)
+state = st.sidebar.multiselect(
+    "Выберите штат:",
+    options=df["State"].unique(),
+)
+city = st.sidebar.multiselect(
+    "Выберите город:",
+    options=df["City"].unique(),
+)
+market = st.sidebar.multiselect(
+    "Выберите магазин:",
+    options=df["Market"].unique(),
+)
+# формирование строки запроса с учетом фильтров
+query_string = "("
+if city:
+    query_string = query_string + "City == @city and "
+if ship_mode:
+    query_string = query_string + "`Ship Mode` == @ship_mode and "
+if country:
+    query_string = query_string + "Country == @country and "
+if state:
+    query_string = query_string + "State == @state and "
+if region:
+    query_string = query_string + "Region == @region and "
+if market:
+    query_string = query_string + "Market == @market and "
+if category:
+    query_string = query_string + "Category == @category"
+query_string = query_string + ")"
+if query_string[-5::] == "and )":
+    query_string = query_string[:len(query_string) - 5:] + ")"
+# применение фильтрации
+df_selection = df.query(query_string)
 
-#
-# data_load_state.text("неЗаполнение пропущенных данных")
-# Заполнение пропущенных данных
-# orders_df.fillna(0, inplace=True)
-# orders_df.fillna(0, inplace=True)
+# Если нет фильтров, то выбрать весь датасет
+if df_selection.empty:
+    df_selection = df
+# выбор цветовой палитры
+color_palette = px.colors.sequential.RdPu
+# Столбчатая диаграмма продаж по подкатегориям
+container1 = st.container()
+with container1:
+    left_column, right_column = st.columns([2, 1])
+    with left_column:
+        st.subheader("Продажи по категориям")
+        data = df_selection.groupby(by=["Sub-Category"], as_index=False)["Sales"].sum()
+        fig_sub_category_sales = px.bar(data, x="Sub-Category", y="Sales", template="plotly_white",
+                                        color_discrete_sequence=[color_palette[3]],
+                                        labels={"Sub-Category": "Категория товаров", "Sales": "Продажи"})
+        st.plotly_chart(fig_sub_category_sales, use_container_width=True)
 
-# data_load_state.text('Цифровизация....')
-# цифровизация категориальных данных
-label_encoder = LabelEncoder()
-orders_df['Ship Mode'] = label_encoder.fit_transform(orders_df['Ship Mode'])
-orders_df['Segment'] = label_encoder.fit_transform(orders_df['Segment'])
-# вот тут под вопросом
-orders_df['Postal Code'] = label_encoder.fit_transform(orders_df['Postal Code'])
-orders_df['City'] = label_encoder.fit_transform(orders_df['City'])
-orders_df['State'] = label_encoder.fit_transform(orders_df['State'])
-orders_df['Country'] = label_encoder.fit_transform(orders_df['Country'])
-orders_df['Region'] = label_encoder.fit_transform(orders_df['Region'])
-orders_df['Market'] = label_encoder.fit_transform(orders_df['Market'])
-orders_df['Category'] = label_encoder.fit_transform(orders_df['Category'])
-orders_df['Sub-Category'] = label_encoder.fit_transform(orders_df['Sub-Category'])
-orders_df['Order Priority'] = label_encoder.fit_transform(orders_df['Order Priority'])
+    with right_column:
+        with st.container():
+            data_style = data.style.background_gradient(cmap='RdPu')
+            st.dataframe(data_style)
 
-# цифровизация н категориальных данных
-# # Инициализация векторизатора
-# vectorizer = CountVectorizer()
-# # Преобразование текста в мешок слов
-# # transform each column individually
-# for column in orders_df.columns:
-#     orders_df[column] = vectorizer.transform([row[column] for row in orders_df])
-#
-#
-# # Пример: преобразуем столбцы с датами в тип datetime
-# orders_df['Order Date'] = pd.to_datetime(orders_df['Order Date'])
-# returns_df['Ship Date'] = pd.to_datetime(orders_df['Ship Date'])
+# Круговая диаграмма продаж по категориям
+container2 = st.container()
+with container2:
+    left_column, right_column = st.columns([2, 1])
+    with left_column:
+        st.subheader("Продажи по категориям")
+        fig_category_country = px.pie(df_selection, values="Sales", names="Category",
+                                      color_discrete_sequence=color_palette,
+                                      labels={"Category": "Категория товаров", "Sales": "Продажи"})
+        fig_category_country.update_traces(text=df_selection["Category"], textposition="inside")
+        st.plotly_chart(fig_category_country, use_container_width=True)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+    with right_column:
+        with st.container():
+            data_style = data.style.background_gradient(cmap='RdPu')
+            st.dataframe(data_style)
 
+# Столбчатая диаграмма продаж по регионам
+container3 = st.container()
+with container3:
+    left_column, right_column = st.columns([2, 1])
+    with left_column:
+        st.subheader("Продажи по регионам")
+        data = df_selection.groupby(by=["Region"], as_index=False)["Sales"].sum()
+        fig_region_sales = px.bar(data, x="Region", y="Sales", template="plotly_white",
+                                  color_discrete_sequence=[color_palette[3]],
+                                  labels={"Region": "Регион", "Sales": "Продажи"})
+        st.plotly_chart(fig_region_sales, use_container_width=True)
 
-@st.experimental_memo
-def load_data():
+    with right_column:
+        with st.container():
+            data_style = data.style.background_gradient(cmap='RdPu')
+            st.dataframe(data_style)
 
-    data = orders_df
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    return data
+# Столбчатая диаграмма возвратов товаров по регионам
+container4 = st.container()
+with container4:
+    left_column, right_column = st.columns([2, 1])
+    with left_column:
+        returns_df = pd.read_excel(file_path, sheet_name='Returns')
+        st.subheader("Возвраты по регионам")
+        df_returned = returns_df.query("(Returned == 'Yes')")
+        data = df_returned.groupby(by=["Region"], as_index=False)["Returned"].count()
+        fig_region_returned = px.bar(data, x="Region", y="Returned", template="plotly_white",
+                                     color_discrete_sequence=[color_palette[3]],
+                                     labels={"Region": "Регион", "Returned": "Количество возвратов"})
+        st.plotly_chart(fig_region_returned, use_container_width=True, height=200)
 
-# Create a text element and let the reader know the data is loading.
-# data_load_state.text('Мы загружаем 10 строчек')
-# Load 10,000 rows of data into the dataframe.
-data = load_data()
-# Notify the reader that the data was successfully loaded.
+    with right_column:
+        data_style = data.style.background_gradient(cmap='RdPu')
+        st.dataframe(data_style)
 
-#
-# # Строим столбчатую диаграмму для примера (замените 'Sales' на нужный столбец)
-# fig, ax = plt.subplots()
-# ax.bar(orders_df['Category'], orders_df['Sales'])
-# ax.set_xlabel('Категория')
-# ax.set_ylabel('Продажи')
-# ax.set_title('Продажи по категориям')
-#
-# # Отображение диаграммы в Streamlit
-# st.pyplot(fig)
+# Динамика продаж во времени
+container5 = st.container()
+with container5:
+    left_column, right_column = st.columns([2, 1])
+    with left_column:
+        df_selection["Date"] = df_selection["Order Date"].dt.to_period("M")
+        st.subheader('Динамика продаж')
+        linechart = pd.DataFrame(
+            df_selection.groupby(df_selection["Date"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+        fig2 = px.line(linechart, x="Date", y="Sales", height=500, width=1000,
+                       template="gridon", color_discrete_sequence=[color_palette[3]],
+                       labels={"Date": "Дата", "Sales": "Продажи"})
+        st.plotly_chart(fig2, use_container_width=True)
 
-# График временного ряда
-st.subheader('Динамика продаж во времени')
-fig_time_series, ax_time_series = plt.subplots()
-ax_time_series.plot(orders_df['Order Date'], orders_df['Sales'])
-ax_time_series.set_xlabel('Дата заказа')
-ax_time_series.set_ylabel('Продажи')
-st.pyplot(fig_time_series)
+    with right_column:
+        data_style = linechart.style.background_gradient(cmap='RdPu')
+        st.dataframe(data_style)
 
-# График со скользящим средним
-st.subheader('Динамика продаж со скользящим средним')
-fig_rolling_mean, ax_rolling_mean = plt.subplots()
-ax_rolling_mean.plot(orders_df['Order Date'], orders_df['Sales'].rolling(window=7).mean(), label='Скользящее среднее')
-ax_rolling_mean.set_xlabel('Дата заказа')
-ax_rolling_mean.set_ylabel('Продажи')
-ax_rolling_mean.legend()
-st.pyplot(fig_rolling_mean)
+# Динамика прибыли во времени
+container6 = st.container()
+with container6:
+    left_column, right_column = st.columns([2, 1])
+    with left_column:
+        st.subheader('Динамика прибыли')
+        linechart = pd.DataFrame(
+            df_selection.groupby(df_selection["Date"].dt.strftime("%Y : %b"))["Profit"].sum()).reset_index()
+        fig2 = px.line(linechart, x="Date", y="Profit", height=500, width=1000,
+                       template="gridon", color_discrete_sequence=[color_palette[3]],
+                       labels={"Date": "Дата", "Profit": "Прибыль"})
+        st.plotly_chart(fig2, use_container_width=True)
 
-# Сравнение категорий
-st.subheader('Сравнение продаж по категориям')
-fig_category_comparison, ax_category_comparison = plt.subplots()
-seaborn.barplot(x='Category', y='Sales', data=orders_df, ax=ax_category_comparison)
-ax_category_comparison.set_xlabel('Категория')
-ax_category_comparison.set_ylabel('Продажи')
-st.pyplot(fig_category_comparison)
-
-# Ящик с усами (Boxplot)
-st.subheader('Распределение продаж по категориям')
-fig_boxplot, ax_boxplot = plt.subplots()
-seaborn.boxplot(x='Category', y='Sales', data=orders_df, ax=ax_boxplot)
-ax_boxplot.set_xlabel('Категория')
-ax_boxplot.set_ylabel('Продажи')
-st.pyplot(fig_boxplot)
-
-# Тепловая карта корреляции
-st.subheader('Тепловая карта корреляции')
-fig_corr_heatmap, ax_corr_heatmap = plt.subplots()
-corr_matrix = orders_df.corr()
-seaborn.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax_corr_heatmap)
-st.pyplot(fig_corr_heatmap)
+    with right_column:
+        data_style = linechart.style.background_gradient(cmap='RdPu')
+        st.dataframe(data_style)
